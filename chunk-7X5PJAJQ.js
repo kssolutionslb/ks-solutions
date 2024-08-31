4ccd6ed407bbae17e5763c07a0586246844cba34
Karam Karam <karamkaram158@gmail.com>
2024-08-31 19:24:26 +0300

    first commit

---
+++ chunk-7X5PJAJQ.js
@@ -0,0 +1,1 @@
+var o={version:"1.0.7",appOwner:"KS Solutions",ownerEmail:"ks_solutions_lb@hotmail.com",snackbarDuration:3e3};export{o as a};
---
+++ chunk-Z7NV7UNL.js
@@ -0,0 +1,1 @@
+function c(t){let e=document.createElement("a");e.href=t,e.target="_blank",e.click()}export{c as a};
