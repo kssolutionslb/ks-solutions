06a1864648f1b937eb3a7bc6fe064fc261aa08fa
karamkaram17 <karam_kbk@hotmail.com>
2025-04-04 18:00:03 +0300

    Release v0.0.35

--- chunk-3R6TOEJS.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-V5UEVI2V.js";var r={production:!1,API_HOST:o.API_HOST.production};export{r as a};
---
+++ chunk-H7FHUFND.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-NZP3AJU7.js";var r={production:!1,API_HOST:o.API_HOST.production};export{r as a};
