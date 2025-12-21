0eea220c15a81fdd1a7558903817838904d82b58
karamkaram17 <karam_kbk@hotmail.com>
2025-12-21 15:01:18 +0200

    Release v1.0.81

---
+++ chunk-QKOR2QJI.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-NDEZYEOM.js";var r={production:!1,API_HOST:o.API_HOST.production};export{r as a};
--- chunk-YXBT6KIZ.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-MIQK74YD.js";var r={production:!1,API_HOST:o.API_HOST.production};export{r as a};
